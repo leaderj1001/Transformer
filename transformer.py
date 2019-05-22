78c89e8995993d9ad150b95ff02044ceebabcff0
myeongjun <leaderj1001@gmail.com>
2019-05-22 23:58:35 +0900

    [Update] add dropout

--- transformer.py
+++ transformer.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 
-import numpy as np
 import math
 
 from config import get_args
@@ -11,11 +10,11 @@ from config import get_args
 # https://towardsdatascience.com/how-to-code-the-transformer-in-pytorch-24db27c8f9ec
 # Thank you :)
 class PositionalEncoding(nn.Module):
-    def __init__(self, embedding_dim, max_len=128):
+    def __init__(self, embedding_dim, sentence_len=128):
         super(PositionalEncoding, self).__init__()
-        self.positionalEncoding = torch.zeros((max_len, embedding_dim))
+        self.positionalEncoding = torch.zeros((sentence_len, embedding_dim))
 
-        for pos in range(0, max_len):
+        for pos in range(0, sentence_len):
             for i in range(0, embedding_dim // 2):
                 self.positionalEncoding[pos, 2 * i] = math.sin(pos / math.pow(10000, 2 * i / embedding_dim))
                 self.positionalEncoding[pos, 2 * i + 1] = math.cos(pos / math.pow(10000, 2 * i / embedding_dim))
@@ -29,14 +28,10 @@ class PositionalEncoding(nn.Module):
 
 
 class ScaledDotProductAttention(nn.Module):
-    def __init__(self, dk, dropout_rate):
+    def __init__(self, dk):
         super(ScaledDotProductAttention, self).__init__()
         self.dk = dk
-        self.dropout_rate = dropout_rate
-
         self.softmax = nn.Softmax(dim=-1)
-        if dropout_rate > 0.0:
-            self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self, query, key, value, mask):
         out = torch.matmul(query, key.transpose(2, 3))
@@ -47,8 +42,6 @@ class ScaledDotProductAttention(nn.Module):
             out = out.masked_fill(mask == 0, -1e10)
         out = self.softmax(out)
 
-        if self.dropout_rate > 0.0:
-            out = self.dropout(out)
         out = torch.matmul(out, value)
 
         return out
@@ -65,20 +58,16 @@ class MultiHeadAttention(nn.Module):
         self.dense_key = nn.Linear(embedding_dim, embedding_dim)
         self.dense_value = nn.Linear(embedding_dim, embedding_dim)
 
-        self.scaled_dot_product_attention = ScaledDotProductAttention(dk=self.dk, dropout_rate=dropout_rate)
+        self.scaled_dot_product_attention = ScaledDotProductAttention(dk=self.dk)
 
         self.dense = nn.Linear(embedding_dim, embedding_dim)
 
     def forward(self, query, key, value, mask=None):
-        batch, max_len, embedding_dim = query.size()
+        batch, _, embedding_dim = query.size()
 
-        query_out = self.dense_query(query).view(batch, -1, self.head, self.dk)
-        key_out = self.dense_key(key).view(batch, -1, self.head, self.dk)
-        value_out = self.dense_value(value).view(batch, -1, self.head, self.dk)
-
-        query_out = query_out.transpose(1, 2)
-        key_out = key_out.transpose(1, 2)
-        value_out = value_out.transpose(1, 2)
+        query_out = self.dense_query(query).view(batch, -1, self.head, self.dk).transpose(1, 2)
+        key_out = self.dense_key(key).view(batch, -1, self.head, self.dk).transpose(1, 2)
+        value_out = self.dense_value(value).view(batch, -1, self.head, self.dk).transpose(1, 2)
 
         out = self.scaled_dot_product_attention(query_out, key_out, value_out, mask).view(batch, -1, self.embedding_dim)
 
@@ -105,26 +94,26 @@ class LayerNorm(nn.Module):
 
 
 class FeedForward(nn.Module):
-    def __init__(self, embedding_dim, dropout):
+    def __init__(self, embedding_dim, dropout_rate):
         super(FeedForward, self).__init__()
         self.embedding_dim = embedding_dim
-        self.dropout = dropout
         self.inner_dim = 2048
 
         # in the paper, they suggest two kind of methods.
         self.dense = nn.Sequential(
             nn.Linear(self.embedding_dim, self.inner_dim),
             nn.ReLU(),
-            nn.Dropout(self.dropout),
+            nn.Dropout(dropout_rate),
             nn.Linear(self.inner_dim, self.embedding_dim)
         )
 
-        self.conv = nn.Sequential(
-            nn.Conv2d(self.embedding_dim, self.inner_dim, kernel_size=1),
-            nn.ReLU(),
-            nn.Dropout(self.dropout),
-            nn.Conv2d(self.inner_dim, self.embedding_dim, kernel_size=1)
-        )
+        # If you use Conv layer, you have to adjust the dimension.
+        # self.conv = nn.Sequential(
+        #     nn.Conv2d(self.embedding_dim, self.inner_dim, kernel_size=1),
+        #     nn.ReLU(),
+        #     nn.Dropout(dropout_rate),
+        #     nn.Conv2d(self.inner_dim, self.embedding_dim, kernel_size=1)
+        # )
 
     def forward(self, x):
         out = self.dense(x)
@@ -144,13 +133,16 @@ class EncoderLayer(nn.Module):
         self.feed_forward = FeedForward(embedding_dim, dropout_rate)
         self.layer_norm2 = LayerNorm(embedding_dim)
 
+        self.dropout1 = nn.Dropout(dropout_rate)
+        self.dropout2 = nn.Dropout(dropout_rate)
+
     def forward(self, x, x_mask):
         # multi head attention
-        multi_head_out = self.multi_head_attention(x, x, x, x_mask)
+        multi_head_out = self.dropout1(self.multi_head_attention(x, x, x, x_mask))
         out = self.layer_norm1(multi_head_out + x)
 
         # feed forward layer
-        feed_forward_out = self.feed_forward(out)
+        feed_forward_out = self.dropout2(self.feed_forward(out))
         out = self.layer_norm2(feed_forward_out + out)
         return out
 
@@ -194,19 +186,23 @@ class DecoderLayer(nn.Module):
         self.layer_norm2 = LayerNorm(embedding_dim)
         self.layer_norm3 = LayerNorm(embedding_dim)
 
+        self.dropout1 = nn.Dropout(dropout_rate)
+        self.dropout2 = nn.Dropout(dropout_rate)
+
         self.feed_forward = FeedForward(embedding_dim, dropout_rate)
+        self.dropout3 = nn.Dropout(dropout_rate)
 
     def forward(self, encoder_out, target, x_mask, target_mask):
         # multi head attention
-        multi_head_out1 = self.multi_head_attention1(target, target, target, target_mask)
+        multi_head_out1 = self.dropout1(self.multi_head_attention1(target, target, target, target_mask))
         out = self.layer_norm1(multi_head_out1 + target)
 
         # multi head attention
-        multi_head_out2 = self.multi_head_attention2(out, encoder_out, encoder_out, x_mask)
+        multi_head_out2 = self.dropout2(self.multi_head_attention2(out, encoder_out, encoder_out, x_mask))
         out = self.layer_norm2(multi_head_out2 + out)
 
         # feed forward layer
-        feed_forward_out = self.feed_forward(out)
+        feed_forward_out = self.dropout3(self.feed_forward(out))
         out = self.layer_norm3(feed_forward_out + out)
 
         return out
@@ -254,26 +250,26 @@ class Transformer(nn.Module):
 
 
 # Test Code
-# def masking(source, target, args):
-#     source_mask = (source != 1).unsqueeze(-2)
-#
-#     target_mask = (target != 1).unsqueeze(-2)
-#     sentence_len = target.size(1)
-#
-#     numpy_mask = torch.tril(torch.ones((1, sentence_len, sentence_len)))
-#     numpy_mask = (numpy_mask == 0).to('cpu')
-#
-#     target_mask = target_mask & numpy_mask
-#
-#     return source_mask, target_mask
-#
-#
-# args = get_args()
-# source = torch.randint(high=100, size=(4, args.max_len))
-# target = torch.randint(high=50, size=(4, args.max_len))
-# target_input = target[:, :-1]
-#
-# source_mask, target_mask = masking(source, target_input, args)
-#
-# transformer = Transformer(100, 50, args.max_len, heads=4, embedding_dim=512, dropout_rate=0.1, N=6)
-# print(transformer(source, source_mask, target_input, target_mask).shape)
+def masking(source, target, args):
+    source_mask = (source != 1).unsqueeze(-2)
+
+    target_mask = (target != 1).unsqueeze(-2)
+    sentence_len = target.size(1)
+
+    numpy_mask = torch.tril(torch.ones((1, sentence_len, sentence_len)))
+    numpy_mask = (numpy_mask == 0).to('cpu')
+
+    target_mask = target_mask & numpy_mask
+
+    return source_mask, target_mask
+
+
+args = get_args()
+source = torch.randint(high=100, size=(4, args.max_len))
+target = torch.randint(high=50, size=(4, args.max_len))
+target_input = target[:, :-1]
+
+source_mask, target_mask = masking(source, target_input, args)
+
+transformer = Transformer(100, 50, args.max_len, heads=4, embedding_dim=512, dropout_rate=0.1, N=6)
+print(transformer(source, source_mask, target_input, target_mask).shape)
